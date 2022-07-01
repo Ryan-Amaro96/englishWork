2e4dbfb1aeccab90c12c3d2dcd56f9b7b0c54768
Ryan Amaro <ryanamaro2003.st@gmail.com>
2022-07-01 10:23:55 -0300

    first´´´commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let comedy =[{
+    title: "21 jump street",
+    director:["Phil Lord","Christopher Miller"] ,
+    year: 2012,
+    },
+    {
+    title:"superbad" ,
+    director: "Greg Mottola",
+    year: 2007,
+    },
+    {
+    title: "Project x",
+    director:"Numa Nourizadeh" ,
+    year: 2012,
+    }
+]
+    
+    let action =[{
+    title:"Fast & Furious 7" ,
+    director: "James Wan",
+    year: 2015,
+    },
+    {
+    title:"The Expendables" ,
+    director: "Sylvester Stallone",
+    year:2010 ,
+    },
+    {
+    title: "Baby driver",
+    director: "Edgar Wright",
+    year: 2017,
+    }
+    ]
+    
+    let fantasy=[{
+    title: "Pirates of The Caribbean: The curse of Black pearl",
+    director:"Gore Verbinski" ,
+    year:2003 ,
+    },
+    {
+    title: "fantastic Beasts and where to fundo them",
+    director: "David Yates",
+    year: 2017 ,
+    },
+    {
+    title:"Bright" ,
+    director:"David Ayer" ,
+    year: 2017,
+    }
+    ]
